e8f6615985d1683bdfe1a65d363f0d2e10fee994
Raymond Kim <rkim@tenstorrent.com>
2024-10-28 17:47:42 +0000

    #14369: Add initial standalone umd build

---
+++ scripts/build_scripts/create_standalone_umd_lib.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# This script will create a standalone UMD artifact with as many static
+# libraries as possible for downstream use. This should ideally be in CI
+# for UMD.
+
+set -euo pipefail
+
+apt install patchelf
+
+cd tt_metal/third_party/umd
+git clean -xffd
+cmake -B build -G Ninja -DTT_UMD_BUILD_TESTS=ON -DBUILD_SHARED_LIBS=OFF
+cmake --build build --target umd_tests
+
+cd build/test/umd
+
+# Remove hardcoded RPATH
+find . -type f | xargs -n 1 -I {} patchelf --remove-rpath {}
+
+mkdir -p device
+mkdir -p lib
+mkdir -p tests
+
+cp ../../lib/libdevice.so lib/
+cp -r ../../_deps/nanomsg-build/*.so* lib/
+cp -r ../../_deps/libuv-build/*.so* lib/
+cp -r ../../../device/bin/ device/
+cp -r ../../../tests/soc_descs/ tests/
+
+cd ../../../
+mv tt_metal/third_party/umd/build/test/umd _umd
