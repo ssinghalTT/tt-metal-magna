344c5d8bc13045e06f7abe0eb2780a8d4fcbd978
Hoang Ngo <nzo8oanz34@gmail.com>
2024-11-11 10:47:17 +0000

    #0: Add missing include

--- ttnn/cpp/ttnn/operations/moreh/moreh_layer_norm_backward/device/kernels/moreh_layer_norm_backward_gamma_beta_grad_kernel.cpp
+++ ttnn/cpp/ttnn/operations/moreh/moreh_layer_norm_backward/device/kernels/moreh_layer_norm_backward_gamma_beta_grad_kernel.cpp
@@ -3,6 +3,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 #include "ttnn/cpp/ttnn/deprecated/tt_dnn/kernels/compute/moreh_common.hpp"
+#include "tools/profiler/kernel_profiler.hpp”
 
 #define LOOP_COUNT 10
 
