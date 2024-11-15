1c576b73a93f9d938436aac55672a8e3c2dc3e12
wransom-TT <wransom@tenstorrent.com>
2024-11-15 18:55:39 +0000

    #0: updated compute to process 1 tile at a time, but hangs, need to update reader accordingly

--- ttnn/cpp/ttnn/operations/pool/maxpool/device/kernels/compute/max_pool_multi_core.cpp
+++ ttnn/cpp/ttnn/operations/pool/maxpool/device/kernels/compute/max_pool_multi_core.cpp
@@ -119,7 +119,7 @@ void MAIN {
 
     constexpr uint32_t MAX_TILES_PER_REDUCTION = 8;
 
-    constexpr uint32_t max_tiles_per_iter = in_ntiles_c < MAX_TILES_PER_REDUCTION ? in_ntiles_c : MAX_TILES_PER_REDUCTION;
+    constexpr uint32_t max_tiles_per_iter = 1;//in_ntiles_c < MAX_TILES_PER_REDUCTION ? in_ntiles_c : MAX_TILES_PER_REDUCTION;
     constexpr uint32_t partial_iter_output_tiles = in_ntiles_c % MAX_TILES_PER_REDUCTION;
     tilizeA_B_reduce_init_short(in_cb_id,
                                 in_scalar_cb_id,
@@ -131,32 +131,37 @@ void MAIN {
 
     cb_wait_front(in_scalar_cb_id, 1);
     for (uint32_t i = 0; i < nsticks_per_core; ++ i) {
-        for (uint32_t b_i = 0; b_i < in_nblocks_c; ++ b_i) {
-            if (b_i == in_nblocks_c - 1 && partial_iter_output_tiles > 0) {
-                //tilize_uninit(in_cb_id, out_cb_id);
-                pack_untilize_uninit(out_cb_id);
-                /* tilizeA_B_reduce_init_short(in_cb_id,
-                                            in_scalar_cb_id,
-                                            partial_iter_output_tiles,
-                                            out_cb_id,
-                                            num_faces_in_tile,
-                                            window_size_hw); */
-                pack_untilize_dst_init_short<partial_iter_output_tiles>(out_cb_id, num_out_rows, num_faces_in_tile); /* pack 1 row (1x16 or 1x32) */
-                reduce_h_fused<partial_iter_output_tiles, is_partial_tile, split_reader, window_size_hw>(in_cb_id, in_scalar_cb_id, i, out_cb_id);
-            }
-            else {
-                //tilize_uninit(in_cb_id, out_cb_id);
-                pack_untilize_uninit(out_cb_id);
-                /* tilizeA_B_reduce_init_short(in_cb_id,
-                                            in_scalar_cb_id,
-                                            max_tiles_per_iter,
-                                            out_cb_id,
-                                            num_faces_in_tile,
-                                            window_size_hw); */
-                pack_untilize_dst_init_short<max_tiles_per_iter>(out_cb_id, num_out_rows, num_faces_in_tile); /* pack 1 row (1x16 or 1x32) */
-                reduce_h_fused<max_tiles_per_iter, is_partial_tile, split_reader, window_size_hw>(in_cb_id, in_scalar_cb_id, i, out_cb_id);
-            }
+        // for (uint32_t b_i = 0; b_i < in_nblocks_c; ++ b_i) {
+        //     if (b_i == in_nblocks_c - 1 && partial_iter_output_tiles > 0) {
+        //         tilize_uninit(in_cb_id, out_cb_id);
+        //         pack_untilize_uninit(out_cb_id);
+        //         tilizeA_B_reduce_init_short(in_cb_id,
+        //                                     in_scalar_cb_id,
+        //                                     partial_iter_output_tiles,
+        //                                     out_cb_id,
+        //                                     num_faces_in_tile,
+        //                                     window_size_hw);
+        //         pack_untilize_dst_init_short<partial_iter_output_tiles>(out_cb_id, num_out_rows, num_faces_in_tile); /* pack 1 row (1x16 or 1x32) */
+        //         reduce_h_fused<partial_iter_output_tiles, is_partial_tile, split_reader, window_size_hw>(in_cb_id, in_scalar_cb_id, i, out_cb_id);
+        //     }
+        //     else {
+        //         tilize_uninit(in_cb_id, out_cb_id);
+        //         pack_untilize_uninit(out_cb_id);
+        //         tilizeA_B_reduce_init_short(in_cb_id,
+        //                                     in_scalar_cb_id,
+        //                                     max_tiles_per_iter,
+        //                                     out_cb_id,
+        //                                     num_faces_in_tile,
+        //                                     window_size_hw);
+        //         pack_untilize_dst_init_short<max_tiles_per_iter>(out_cb_id, num_out_rows, num_faces_in_tile); /* pack 1 row (1x16 or 1x32) */
+        //         reduce_h_fused<max_tiles_per_iter, is_partial_tile, split_reader, window_size_hw>(in_cb_id, in_scalar_cb_id, i, out_cb_id);
+        //     }
+        // }
+
+        for (uint32_t i = 0; i < in_ntiles_c; ++i) {
+            reduce_h_fused<max_tiles_per_iter, is_partial_tile, split_reader, window_size_hw>(in_cb_id, in_scalar_cb_id, i, out_cb_id);
         }
+
     }
     cb_pop_front(in_scalar_cb_id, 1);
 }
