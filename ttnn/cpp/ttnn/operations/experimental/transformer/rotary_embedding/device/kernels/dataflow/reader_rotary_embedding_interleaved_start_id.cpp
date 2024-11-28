273a40d465e0f141f230f0ab9ffb2d87bf0eb82f
Anil Mahmud <amahmudTT@tenstorrent.com>
2024-11-28 19:42:00 +0000

    Reduce test : Remove cosine part

--- ttnn/cpp/ttnn/operations/experimental/transformer/rotary_embedding/device/kernels/dataflow/reader_rotary_embedding_interleaved_start_id.cpp
+++ ttnn/cpp/ttnn/operations/experimental/transformer/rotary_embedding/device/kernels/dataflow/reader_rotary_embedding_interleaved_start_id.cpp
@@ -7,7 +7,6 @@
 
 void kernel_main() {
     uint32_t src_addr = get_arg_val<uint32_t>(0);
-    uint32_t cos_addr = get_arg_val<uint32_t>(1);
     uint32_t sin_addr = get_arg_val<uint32_t>(2);
     uint32_t num_rows = get_arg_val<uint32_t>(3);
     uint32_t start_id = get_arg_val<uint32_t>(4);
@@ -16,11 +15,9 @@ void kernel_main() {
 
     constexpr uint32_t input_cb_id = get_compile_time_arg_val(0);
     constexpr uint32_t rotated_input_cb_id = get_compile_time_arg_val(1);
-    constexpr uint32_t cos_cb_id = get_compile_time_arg_val(2);
     constexpr uint32_t sin_cb_id = get_compile_time_arg_val(3);
     constexpr uint32_t scalar_cb_id = get_compile_time_arg_val(4);
     constexpr bool input_is_dram = get_compile_time_arg_val(5) == 1;
-    constexpr bool cos_is_dram = get_compile_time_arg_val(6) == 1;
     constexpr bool sin_is_dram = get_compile_time_arg_val(7) == 1;
     constexpr uint16_t scalar_value = get_compile_time_arg_val(8);
     constexpr uint32_t Ht = get_compile_time_arg_val(9);
@@ -34,12 +31,6 @@ void kernel_main() {
 
     const InterleavedAddrGenFast<input_is_dram> s0 = {
         .bank_base_address = src_addr, .page_size = input_tile_bytes, .data_format = input_data_format};
-
-    const uint32_t cos_tile_bytes = get_tile_size(cos_cb_id);
-    const DataFormat cos_data_format = get_dataformat(cos_cb_id);
-
-    const InterleavedAddrGenFast<cos_is_dram> s1 = {
-        .bank_base_address = cos_addr, .page_size = cos_tile_bytes, .data_format = cos_data_format};
 
     const uint32_t sin_tile_bytes = get_tile_size(sin_cb_id);
     const DataFormat sin_data_format = get_dataformat(sin_cb_id);
@@ -61,22 +52,15 @@ void kernel_main() {
     uint32_t cos_sin_curr_id = cos_sin_start_id;
     uint32_t ht = start_row_id;
 
-#ifdef DECODE_MODE
     cb_reserve_back(sin_cb_id, Wt);
-    cb_reserve_back(cos_cb_id, Wt);
     uint32_t sin_l1_write_addr = get_write_ptr(sin_cb_id);
-    uint32_t cos_l1_write_addr = get_write_ptr(cos_cb_id);
     for (uint32_t i = 0; i < Wt; i++) {
         noc_async_read_tile(cos_sin_curr_id, s2, sin_l1_write_addr);
-        noc_async_read_tile(cos_sin_curr_id, s1, cos_l1_write_addr);
         cos_sin_curr_id++;
         sin_l1_write_addr += sin_tile_bytes;
-        cos_l1_write_addr += cos_tile_bytes;
     }
     noc_async_read_barrier();
     cb_push_back(sin_cb_id, Wt);
-    cb_push_back(cos_cb_id, Wt);
-#endif
 
     // read a ublock of tiles from src to CB, and then push the ublock to unpacker
     for (uint32_t i = 0; i < num_rows; ++i) {
@@ -88,13 +72,11 @@ void kernel_main() {
             cb_push_back(rotated_input_cb_id, onetile);
             rotated_input_curr_id++;
 
-#ifndef DECODE_MODE
             cb_reserve_back(sin_cb_id, onetile);
             uint32_t sin_l1_write_addr = get_write_ptr(sin_cb_id);
             noc_async_read_tile(cos_sin_curr_id, s2, sin_l1_write_addr);
             noc_async_read_barrier();
             cb_push_back(sin_cb_id, onetile);
-#endif
 
             cb_reserve_back(input_cb_id, onetile);
             uint32_t input_l1_write_addr = get_write_ptr(input_cb_id);
@@ -103,26 +85,15 @@ void kernel_main() {
             cb_push_back(input_cb_id, onetile);
             input_curr_id++;
 
-#ifndef DECODE_MODE
-            cb_reserve_back(cos_cb_id, onetile);
-            uint32_t cos_l1_write_addr = get_write_ptr(cos_cb_id);
-            noc_async_read_tile(cos_sin_curr_id, s1, cos_l1_write_addr);
-            noc_async_read_barrier();
-            cb_push_back(cos_cb_id, onetile);
-            cos_sin_curr_id++;
-#endif
-
             if (j == half_Wt - 1) {
                 rotated_input_curr_id -= Wt;
             }
         }
         rotated_input_curr_id += Wt;
-#ifndef DECODE_MODE
         ht++;
         if (ht == Ht) {
             ht = 0;
             cos_sin_curr_id -= HtWt;
         }
-#endif
     }
 }
--- ttnn/cpp/ttnn/operations/experimental/transformer/rotary_embedding/device/kernels/dataflow/writer_rotary_embedding_interleaved_start_id.cpp
+++ ttnn/cpp/ttnn/operations/experimental/transformer/rotary_embedding/device/kernels/dataflow/writer_rotary_embedding_interleaved_start_id.cpp
@@ -15,21 +15,16 @@ void kernel_main() {
     // single-tile ublocks
     constexpr uint32_t onetile = 1;
 
-#ifndef OUT_SHARDED
     const uint32_t tile_bytes = get_tile_size(cb_id_out);
     const DataFormat data_format = get_dataformat(cb_id_out);
 
     const InterleavedAddrGenFast<dst_is_dram> s = {
         .bank_base_address = dst_addr, .page_size = tile_bytes, .data_format = data_format};
-#endif
 
-#ifdef DECODE_MODE
     uint32_t cos_sin_offset = get_arg_val<uint32_t>(3);
     uint32_t Wt = get_arg_val<uint32_t>(4);
     uint32_t Wbytes = get_arg_val<uint32_t>(5);
 
-    constexpr uint32_t untilized_cos_cb_id = get_compile_time_arg_val(2);
-    constexpr uint32_t untilized_cos_sync_cb_id = get_compile_time_arg_val(3);
     constexpr uint32_t untilized_sin_cb_id = get_compile_time_arg_val(4);
     constexpr uint32_t untilized_sin_sync_cb_id = get_compile_time_arg_val(5);
     cb_wait_front(untilized_sin_cb_id, Wt);
@@ -40,18 +35,6 @@ void kernel_main() {
     noc_async_read_barrier();
     cb_push_back(untilized_sin_sync_cb_id, Wt);
 
-    cb_wait_front(untilized_cos_cb_id, Wt);
-    cb_reserve_back(untilized_cos_sync_cb_id, Wt);
-    uint64_t cos_l1_read_addr = get_noc_addr(get_read_ptr(untilized_cos_cb_id)) + cos_sin_offset;
-    uint32_t cos_l1_write_addr = get_read_ptr(untilized_cos_cb_id);
-    noc_async_read(cos_l1_read_addr, cos_l1_write_addr, Wbytes);
-    noc_async_read_barrier();
-    cb_push_back(untilized_cos_sync_cb_id, Wt);
-#endif
-
-#ifdef OUT_SHARDED
-    cb_wait_front(cb_id_out, num_tiles);
-#else
     uint32_t end_id = start_id + num_tiles;
     for (uint32_t i = start_id; i < end_id; ++i) {
         cb_wait_front(cb_id_out, onetile);
@@ -63,5 +46,4 @@ void kernel_main() {
 
         cb_pop_front(cb_id_out, onetile);
     }
-#endif
 }
