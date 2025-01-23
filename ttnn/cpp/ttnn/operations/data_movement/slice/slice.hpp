f88d6bde0acde2227523517587002583b897d3cd
MohammedTaherMcW <mohammed.rasheed@multicorewareinc.com>
2025-01-23 18:19:04 +0530

    Added header files to support Tensor input for indexing in slice Operator

--- ttnn/cpp/ttnn/operations/data_movement/slice/slice.hpp
+++ ttnn/cpp/ttnn/operations/data_movement/slice/slice.hpp
@@ -92,6 +92,24 @@ struct SliceOperation {
         const std::optional<MemoryConfig>& memory_config_arg = std::nullopt,
         const std::optional<Tensor>& optional_output_tensor = std::nullopt,
         const std::optional<float>& pad_value = std::nullopt);
+        const std::optional<Tensor>& optional_output_tensor = std::nullopt);
+
+    static ttnn::Tensor invoke(
+        uint8_t queue_id,
+        const ttnn::Tensor& input_tensor,
+        const ttnn::Tensor& output_tensor_start,
+        const ttnn::Tensor& output_tensor_end,
+        const std::optional<ttnn::SmallVector<int>>& step,
+        const std::optional<MemoryConfig>& memory_config_arg = std::nullopt,
+        const std::optional<Tensor>& optional_output_tensor = std::nullopt);
+
+    static ttnn::Tensor invoke(
+        const ttnn::Tensor& input_tensor,
+        const ttnn::Tensor& output_tensor_start,
+        const ttnn::Tensor& output_tensor_end,
+        const std::optional<ttnn::SmallVector<int>>& step,
+        const std::optional<MemoryConfig>& memory_config_arg = std::nullopt,
+        const std::optional<Tensor>& optional_output_tensor = std::nullopt);
 };
 
 }  // namespace data_movement
