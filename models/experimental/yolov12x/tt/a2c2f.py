rayraykay/tt-metal

#18636: Yolov12x model ttnn bring up

models/experimental/functional_yolov12x/tt/a2c2f.py
@@ -1,0 +1,97 @@
+# SPDX-FileCopyrightText: © 2025 Tenstorrent Inc.
+
+# SPDX-License-Identifier: Apache-2.0
+
+
+import ttnn
+from models.experimental.yolo_common.yolo_utils import concat
+from models.experimental.functional_yolov12x.tt.c3k2 import C3k
+from models.experimental.functional_yolov12x.tt.ablock import ABlock
+from models.experimental.functional_yolov12x.tt.common import Yolov12x_Conv2D
+
+
+class A2C2f:
+    def __init__(
+        self,
+        device,
+        parameter,
+        conv_pt,
+        c1,
+        c2,
+        n=1,
+        a2=True,
+        area=1,
+        residual=False,
+        mlp_ratio=2.0,
+        e=0.5,
+        g=1,
+        shortcut=True,
+        use_1d_systolic_array=True,
+        shard_layout=ttnn.TensorMemoryLayout.HEIGHT_SHARDED,
+        config_override=None,
+    ):
+        residual = True
+        self.m = []
+
+        self.n = n
+        self.a2 = a2
+        self.area = area
+        self.cv1 = Yolov12x_Conv2D(conv=parameter.cv1.conv, conv_pth=conv_pt.cv1.conv, device=device, activation="silu")
+        self.cv2 = Yolov12x_Conv2D(
+            conv=parameter.cv2.conv,
+            conv_pth=conv_pt.cv2.conv,
+            device=device,
+            activation="silu",
+            use_1d_systolic_array=use_1d_systolic_array,
+            shard_layout=shard_layout,
+            config_override=config_override,
+        )  # , use_1d_systolic_array=False, shard_layout=ttnn.TensorMemoryLayout.BLOCK_SHARDED, config_override={"act_block_h": 32}
+        if a2 and residual:
+            self.gamma = 0.01 * ttnn.ones(
+                [c2], dtype=ttnn.bfloat16, device=device, layout=ttnn.TILE_LAYOUT, memory_config=ttnn.L1_MEMORY_CONFIG
+            )
+        else:
+            self.gamma = None
+        if a2:
+            for i in range(2):
+                layers = ABlock(
+                    device,
+                    parameter[i][i],
+                    conv_pt.m[i][i],
+                    dim=384,
+                    num_heads=12,
+                    mlp_ratio=1.2,
+                    area=area,
+                    is_bk_enabled=False,
+                )
+                self.m.append(layers)
+        else:
+            self.m.append(C3k(device, parameter[0], conv_pt.m[0]))
+
+    def __call__(self, x):
+        y = [self.cv1(x)]
+        for _ in range(self.n):
+            if self.a2:
+                for m in self.m:
+                    a0 = m(y[-1])
+                    a1 = m(a0)
+                y.append(a1)
+            else:
+                for m in self.m:
+                    y.append(m(y[-1]))
+        y_concat = concat(-1, True, *y)
+
+        y_concat = ttnn.sharded_to_interleaved(y_concat, ttnn.L1_MEMORY_CONFIG)
+
+        y = self.cv2(y_concat)
+        if self.gamma is not None:
+            if x.is_sharded():
+                x = ttnn.sharded_to_interleaved(x, ttnn.L1_MEMORY_CONFIG)
+            if y.is_sharded():
+                y = ttnn.sharded_to_interleaved(y, ttnn.L1_MEMORY_CONFIG)
+            gamma = ttnn.unsqueeze_to_4D(self.gamma)
+            y = gamma * y
+            ttnn.deallocate(gamma)
+            return x + y
+
+        return y

models/experimental/functional_yolov12x/tt/aattn.py
@@ -1,0 +1,82 @@
+# SPDX-FileCopyrightText: © 2025 Tenstorrent Inc.
+
+# SPDX-License-Identifier: Apache-2.0
+
+
+import ttnn
+from models.experimental.functional_yolov12x.tt.common import Yolov12x_Conv2D
+
+
+class AAttn:
+    def __init__(self, device, parameter, conv_pt, dim=384, num_heads=8, area=1, is_bk_enabled=False):
+        self.area = area
+        self.num_heads = num_heads
+        self.head_dim = dim // num_heads
+        all_head_dim = self.head_dim * self.num_heads
+        self.scale = self.head_dim**-0.5
+
+        self.qkv = Yolov12x_Conv2D(conv=parameter.qkv.conv, conv_pth=conv_pt.qkv.conv, device=device)
+        self.proj = Yolov12x_Conv2D(conv=parameter.proj.conv, conv_pth=conv_pt.proj.conv, device=device)
+        self.pe = Yolov12x_Conv2D(
+            conv=parameter.pe.conv,
+            conv_pth=conv_pt.pe.conv,
+            device=device,
+            config_override={"act_block_h": 32},
+            use_1d_systolic_array=False,
+            shard_layout=ttnn.TensorMemoryLayout.BLOCK_SHARDED,
+        )
+
+    def __call__(self, x):
+        batch_size, qkv_height, qkv_width, qkv_chan = x.shape
+        qkv_n = qkv_height * qkv_width
+        qkv = self.qkv(x)
+        if qkv.is_sharded():
+            qkv = ttnn.sharded_to_interleaved(qkv, ttnn.L1_MEMORY_CONFIG)
+        qkv = ttnn.to_layout(qkv, ttnn.ROW_MAJOR_LAYOUT)
+
+        if self.area > 1:
+            qkv = ttnn.reshape(qkv, (batch_size * self.area, qkv_chan * 3, qkv_n // self.area))
+            batch_size, _, qkv_n = qkv.shape
+
+        # Using ttnn.reshape instead of view as "The last dimension can not change in view"
+        qkv = ttnn.reshape(qkv, (batch_size, qkv_n, self.num_heads, self.head_dim * 3))
+        qkv = ttnn.permute(qkv, (0, 2, 3, 1))
+        q, k, v = ttnn.split(qkv, qkv.shape[2] // 3, 2)
+        ttnn.deallocate(qkv)
+
+        q = ttnn.permute(q, (0, 1, 3, 2))
+
+        q = ttnn.to_layout(q, ttnn.TILE_LAYOUT)
+        k = ttnn.to_layout(k, ttnn.TILE_LAYOUT)
+        attn = ttnn.matmul(q, k, memory_config=ttnn.L1_MEMORY_CONFIG)
+
+        ttnn.deallocate(q)
+        ttnn.deallocate(k)
+
+        attn = ttnn.multiply(attn, self.scale)
+        attn = ttnn.softmax(attn, dim=-1)
+        attn = ttnn.permute(attn, (0, 1, 3, 2))
+
+        v = ttnn.to_layout(v, ttnn.TILE_LAYOUT)
+        attn = ttnn.to_layout(attn, ttnn.TILE_LAYOUT)
+        x = ttnn.matmul(v, attn, memory_config=ttnn.L1_MEMORY_CONFIG)
+        ttnn.deallocate(attn)
+
+        x = ttnn.permute(x, (0, 3, 1, 2))
+        v = ttnn.permute(v, (0, 3, 1, 2))
+
+        if self.area > 1:
+            x = ttnn.reshape(x, (1, batch_size // self.area, qkv_n * self.area, qkv_chan))
+            v = ttnn.reshape(v, (1, batch_size // self.area, qkv_n * self.area, qkv_chan))
+            batch_size, qkv_n, _, _ = x.shape
+
+        x = ttnn.reshape(x, (batch_size, qkv_height, qkv_width, qkv_chan))
+        v = ttnn.reshape(v, (batch_size, qkv_height, qkv_width, qkv_chan))
+
+        y = self.pe(v)
+        x = x + y
+        ttnn.deallocate(v)
+
+        x = self.proj(x)
+
+        return x

models/experimental/functional_yolov12x/tt/ablock.py
@@ -1,0 +1,33 @@
+# SPDX-FileCopyrightText: © 2025 Tenstorrent Inc.
+
+# SPDX-License-Identifier: Apache-2.0
+
+
+from models.experimental.functional_yolov12x.tt.aattn import AAttn
+from models.experimental.functional_yolov12x.tt.common import Yolov12x_Conv2D
+
+
+class ABlock:
+    def __init__(self, device, parameter, conv_pt, dim=384, num_heads=12, mlp_ratio=1.2, area=1, is_bk_enabled=False):
+        self.area = area
+        self.num_heads = num_heads
+        self.head_dim = dim // num_heads
+        all_head_dim = self.head_dim * self.num_heads
+        self.scale = self.head_dim**-0.5
+
+        self.attn = AAttn(
+            device, parameter.attn, conv_pt.attn, dim=dim, num_heads=num_heads, area=area, is_bk_enabled=False
+        )
+        mlp_hidden_dim = dim * mlp_ratio
+        self.mlp_0 = Yolov12x_Conv2D(
+            conv=parameter.mlp[0].conv, conv_pth=conv_pt.mlp[0].conv, device=device, activation="silu"
+        )
+        self.mlp_1 = Yolov12x_Conv2D(
+            conv=parameter.mlp[1].conv, conv_pth=conv_pt.mlp[1].conv, device=device, activation="silu"
+        )
+
+    def __call__(self, x):
+        x = x + self.attn(x)
+        x_0 = self.mlp_0(x)
+        x = x + self.mlp_1(x_0)
+        return x

models/experimental/functional_yolov12x/tt/c3k2.py
@@ -1,0 +1,97 @@
+# SPDX-FileCopyrightText: © 2025 Tenstorrent Inc.
+
+# SPDX-License-Identifier: Apache-2.0
+
+
+import ttnn
+from models.experimental.yolo_common.yolo_utils import concat
+from models.experimental.functional_yolov12x.tt.common import Yolov12x_Conv2D, Bottleneck
+
+
+class C3k:
+    def __init__(self, device, parameter, conv_pt):
+        self.cv1 = Yolov12x_Conv2D(conv=parameter.cv1.conv, conv_pth=conv_pt.cv1.conv, device=device, activation="silu")
+        self.cv2 = Yolov12x_Conv2D(conv=parameter.cv2.conv, conv_pth=conv_pt.cv2.conv, device=device, activation="silu")
+        self.cv3 = Yolov12x_Conv2D(conv=parameter.cv3.conv, conv_pth=conv_pt.cv3.conv, device=device, activation="silu")
+        self.k1 = Bottleneck(device, parameter.m[0], conv_pt.m[0])
+        self.k2 = Bottleneck(device, parameter.m[1], conv_pt.m[1])
+
+    def __call__(self, x):
+        x1 = self.cv1(x)
+        k1 = self.k1(x1)
+        k2 = self.k2(k1)
+        x2 = self.cv2(x)
+
+        x = concat(-1, True, k2, x2)
+        x = self.cv3(x)
+
+        ttnn.deallocate(x1)
+        ttnn.deallocate(x2)
+        ttnn.deallocate(k1)
+        ttnn.deallocate(k2)
+
+        return x
+
+
+class C3k2:
+    def __init__(
+        self,
+        device,
+        parameter,
+        conv_pt,
+        is_bk_enabled=False,
+        use_1d_systolic_array=True,
+        shard_layout=ttnn.TensorMemoryLayout.HEIGHT_SHARDED,
+    ):
+        self.parameter = parameter
+        self.is_bk_enabled = is_bk_enabled
+        conv_config_override = True if conv_pt.cv1.conv.weight.shape[1] == 1536 else False
+        config_override = {"act_block_h": 32} if conv_config_override else None
+
+        self.cv1 = Yolov12x_Conv2D(
+            conv=parameter.cv1.conv,
+            conv_pth=conv_pt.cv1.conv,
+            device=device,
+            config_override=config_override,
+            activation="silu",
+            use_1d_systolic_array=use_1d_systolic_array,
+            shard_layout=shard_layout,
+        )
+        self.cv2 = Yolov12x_Conv2D(
+            conv=parameter.cv2.conv,
+            conv_pth=conv_pt.cv2.conv,
+            device=device,
+            activation="silu",
+            use_1d_systolic_array=use_1d_systolic_array,
+            shard_layout=shard_layout,
+            config_override=config_override,
+        )
+
+        if is_bk_enabled:
+            self.m = Bottleneck(device, parameter[0], conv_pt.m[0])
+        else:
+            self.m_0 = C3k(device, parameter[0], conv_pt.m[0])
+            self.m_1 = C3k(device, parameter[1], conv_pt.m[1])
+
+    def __call__(self, x):
+        x = self.cv1(x)
+        if x.is_sharded():
+            x = ttnn.sharded_to_interleaved(x, ttnn.L1_MEMORY_CONFIG)
+
+        y1 = x[:, :, :, : x.shape[-1] // 2]
+        y2 = x[:, :, :, x.shape[-1] // 2 : x.shape[-1]]
+
+        if self.is_bk_enabled:
+            y3 = self.m(y2)
+            x = concat(-1, False, y1, y2, y3)
+        else:
+            y3 = self.m_0(y2)
+            y4 = self.m_1(y2)
+            x = concat(-1, False, y1, y2, y3, y4)
+
+        ttnn.deallocate(y1)
+        ttnn.deallocate(y2)
+        ttnn.deallocate(y3)
+
+        x = self.cv2(x)
+        return x

ssinghalTT/tt-metal-magna

models/experimental/yolov12x/tt/a2c2f.py
@@ -1,0 +1,97 @@
+# SPDX-FileCopyrightText: © 2025 Tenstorrent Inc.
+
+# SPDX-License-Identifier: Apache-2.0
+
+
+import ttnn
+from models.experimental.yolo_common.yolo_utils import concat
+from models.experimental.yolov12x.tt.c3k2 import C3k
+from models.experimental.yolov12x.tt.ablock import ABlock
+from models.experimental.yolov12x.tt.common import Yolov12x_Conv2D
+
+
+class A2C2f:
+    def __init__(
+        self,
+        device,
+        parameter,
+        conv_pt,
+        c1,
+        c2,
+        n=1,
+        a2=True,
+        area=1,
+        residual=False,
+        mlp_ratio=2.0,
+        e=0.5,
+        g=1,
+        shortcut=True,
+        use_1d_systolic_array=True,
+        shard_layout=ttnn.TensorMemoryLayout.HEIGHT_SHARDED,
+        config_override=None,
+    ):
+        residual = True
+        self.m = []
+
+        self.n = n
+        self.a2 = a2
+        self.area = area
+        self.cv1 = Yolov12x_Conv2D(conv=parameter.cv1.conv, conv_pth=conv_pt.cv1.conv, device=device, activation="silu")
+        self.cv2 = Yolov12x_Conv2D(
+            conv=parameter.cv2.conv,
+            conv_pth=conv_pt.cv2.conv,
+            device=device,
+            activation="silu",
+            use_1d_systolic_array=use_1d_systolic_array,
+            shard_layout=shard_layout,
+            config_override=config_override,
+        )
+        if a2 and residual:
+            self.gamma = 0.01 * ttnn.ones(
+                [c2], dtype=ttnn.bfloat16, device=device, layout=ttnn.TILE_LAYOUT, memory_config=ttnn.L1_MEMORY_CONFIG
+            )
+        else:
+            self.gamma = None
+        if a2:
+            for i in range(2):
+                layers = ABlock(
+                    device,
+                    parameter[i][i],
+                    conv_pt.m[i][i],
+                    dim=384,
+                    num_heads=12,
+                    mlp_ratio=1.2,
+                    area=area,
+                    is_bk_enabled=False,
+                )
+                self.m.append(layers)
+        else:
+            self.m.append(C3k(device, parameter[0], conv_pt.m[0]))
+
+    def __call__(self, x):
+        y = [self.cv1(x)]
+        for _ in range(self.n):
+            if self.a2:
+                for m in self.m:
+                    a0 = m(y[-1])
+                    a1 = m(a0)
+                y.append(a1)
+            else:
+                for m in self.m:
+                    y.append(m(y[-1]))
+        y_concat = concat(-1, True, *y)
+
+        y_concat = ttnn.sharded_to_interleaved(y_concat, ttnn.L1_MEMORY_CONFIG)
+
+        y = self.cv2(y_concat)
+        if self.gamma is not None:
+            if x.is_sharded():
+                x = ttnn.sharded_to_interleaved(x, ttnn.L1_MEMORY_CONFIG)
+            if y.is_sharded():
+                y = ttnn.sharded_to_interleaved(y, ttnn.L1_MEMORY_CONFIG)
+            gamma = ttnn.unsqueeze_to_4D(self.gamma)
+            y = gamma * y
+            ttnn.deallocate(gamma)
+            return x + y
+
+        return y

models/experimental/yolov12x/tt/aattn.py
@@ -1,0 +1,82 @@
+# SPDX-FileCopyrightText: © 2025 Tenstorrent Inc.
+
+# SPDX-License-Identifier: Apache-2.0
+
+
+import ttnn
+from models.experimental.yolov12x.tt.common import Yolov12x_Conv2D
+
+
+class AAttn:
+    def __init__(self, device, parameter, conv_pt, dim=384, num_heads=8, area=1, is_bk_enabled=False):
+        self.area = area
+        self.num_heads = num_heads
+        self.head_dim = dim // num_heads
+        all_head_dim = self.head_dim * self.num_heads
+        self.scale = self.head_dim**-0.5
+
+        self.qkv = Yolov12x_Conv2D(conv=parameter.qkv.conv, conv_pth=conv_pt.qkv.conv, device=device)
+        self.proj = Yolov12x_Conv2D(conv=parameter.proj.conv, conv_pth=conv_pt.proj.conv, device=device)
+        self.pe = Yolov12x_Conv2D(
+            conv=parameter.pe.conv,
+            conv_pth=conv_pt.pe.conv,
+            device=device,
+            config_override={"act_block_h": 32},
+            use_1d_systolic_array=False,
+            shard_layout=ttnn.TensorMemoryLayout.BLOCK_SHARDED,
+        )
+
+    def __call__(self, x):
+        batch_size, qkv_height, qkv_width, qkv_chan = x.shape
+        qkv_n = qkv_height * qkv_width
+        qkv = self.qkv(x)
+        if qkv.is_sharded():
+            qkv = ttnn.sharded_to_interleaved(qkv, ttnn.L1_MEMORY_CONFIG)
+        qkv = ttnn.to_layout(qkv, ttnn.ROW_MAJOR_LAYOUT)
+
+        if self.area > 1:
+            qkv = ttnn.reshape(qkv, (batch_size * self.area, qkv_chan * 3, qkv_n // self.area))
+            batch_size, _, qkv_n = qkv.shape
+
+        # Using ttnn.reshape instead of view as "The last dimension can not change in view"
+        qkv = ttnn.reshape(qkv, (batch_size, qkv_n, self.num_heads, self.head_dim * 3))
+        qkv = ttnn.permute(qkv, (0, 2, 3, 1))
+        q, k, v = ttnn.split(qkv, qkv.shape[2] // 3, 2)
+        ttnn.deallocate(qkv)
+
+        q = ttnn.permute(q, (0, 1, 3, 2))
+
+        q = ttnn.to_layout(q, ttnn.TILE_LAYOUT)
+        k = ttnn.to_layout(k, ttnn.TILE_LAYOUT)
+        attn = ttnn.matmul(q, k)
+
+        ttnn.deallocate(q)
+        ttnn.deallocate(k)
+
+        attn = ttnn.multiply(attn, self.scale)
+        attn = ttnn.softmax(attn, dim=-1)
+        attn = ttnn.permute(attn, (0, 1, 3, 2))
+
+        v = ttnn.to_layout(v, ttnn.TILE_LAYOUT)
+        attn = ttnn.to_layout(attn, ttnn.TILE_LAYOUT)
+        x = ttnn.matmul(v, attn, memory_config=ttnn.L1_MEMORY_CONFIG)
+        ttnn.deallocate(attn)
+
+        x = ttnn.permute(x, (0, 3, 1, 2))
+        v = ttnn.permute(v, (0, 3, 1, 2))
+
+        if self.area > 1:
+            x = ttnn.reshape(x, (1, batch_size // self.area, qkv_n * self.area, qkv_chan))
+            v = ttnn.reshape(v, (1, batch_size // self.area, qkv_n * self.area, qkv_chan))
+            batch_size, qkv_n, _, _ = x.shape
+
+        x = ttnn.reshape(x, (batch_size, qkv_height, qkv_width, qkv_chan))
+        v = ttnn.reshape(v, (batch_size, qkv_height, qkv_width, qkv_chan))
+
+        y = self.pe(v)
+        x = x + y
+        ttnn.deallocate(v)
+
+        x = self.proj(x)
+
+        return x

models/experimental/yolov12x/tt/ablock.py
@@ -1,0 +1,33 @@
+# SPDX-FileCopyrightText: © 2025 Tenstorrent Inc.
+
+# SPDX-License-Identifier: Apache-2.0
+
+
+from models.experimental.yolov12x.tt.aattn import AAttn
+from models.experimental.yolov12x.tt.common import Yolov12x_Conv2D
+
+
+class ABlock:
+    def __init__(self, device, parameter, conv_pt, dim=384, num_heads=12, mlp_ratio=1.2, area=1, is_bk_enabled=False):
+        self.area = area
+        self.num_heads = num_heads
+        self.head_dim = dim // num_heads
+        all_head_dim = self.head_dim * self.num_heads
+        self.scale = self.head_dim**-0.5
+
+        self.attn = AAttn(
+            device, parameter.attn, conv_pt.attn, dim=dim, num_heads=num_heads, area=area, is_bk_enabled=False
+        )
+        mlp_hidden_dim = dim * mlp_ratio
+        self.mlp_0 = Yolov12x_Conv2D(
+            conv=parameter.mlp[0].conv, conv_pth=conv_pt.mlp[0].conv, device=device, activation="silu"
+        )
+        self.mlp_1 = Yolov12x_Conv2D(
+            conv=parameter.mlp[1].conv, conv_pth=conv_pt.mlp[1].conv, device=device, activation="silu"
+        )
+
+    def __call__(self, x):
+        x = x + self.attn(x)
+        x_0 = self.mlp_0(x)
+        x = x + self.mlp_1(x_0)
+        return x

models/experimental/yolov12x/tt/c3k2.py
@@ -1,0 +1,97 @@
+# SPDX-FileCopyrightText: © 2025 Tenstorrent Inc.
+
+# SPDX-License-Identifier: Apache-2.0
+
+
+import ttnn
+from models.experimental.yolo_common.yolo_utils import concat
+from models.experimental.yolov12x.tt.common import Yolov12x_Conv2D, Bottleneck
+
+
+class C3k:
+    def __init__(self, device, parameter, conv_pt):
+        self.cv1 = Yolov12x_Conv2D(conv=parameter.cv1.conv, conv_pth=conv_pt.cv1.conv, device=device, activation="silu")
+        self.cv2 = Yolov12x_Conv2D(conv=parameter.cv2.conv, conv_pth=conv_pt.cv2.conv, device=device, activation="silu")
+        self.cv3 = Yolov12x_Conv2D(conv=parameter.cv3.conv, conv_pth=conv_pt.cv3.conv, device=device, activation="silu")
+        self.k1 = Bottleneck(device, parameter.m[0], conv_pt.m[0])
+        self.k2 = Bottleneck(device, parameter.m[1], conv_pt.m[1])
+
+    def __call__(self, x):
+        x1 = self.cv1(x)
+        k1 = self.k1(x1)
+        k2 = self.k2(k1)
+        x2 = self.cv2(x)
+
+        x = concat(-1, True, k2, x2)
+        x = self.cv3(x)
+
+        ttnn.deallocate(x1)
+        ttnn.deallocate(x2)
+        ttnn.deallocate(k1)
+        ttnn.deallocate(k2)
+
+        return x
+
+
+class C3k2:
+    def __init__(
+        self,
+        device,
+        parameter,
+        conv_pt,
+        is_bk_enabled=False,
+        use_1d_systolic_array=True,
+        shard_layout=ttnn.TensorMemoryLayout.HEIGHT_SHARDED,
+    ):
+        self.parameter = parameter
+        self.is_bk_enabled = is_bk_enabled
+        conv_config_override = True if conv_pt.cv1.conv.weight.shape[1] == 1536 else False
+        config_override = {"act_block_h": 32} if conv_config_override else None
+
+        self.cv1 = Yolov12x_Conv2D(
+            conv=parameter.cv1.conv,
+            conv_pth=conv_pt.cv1.conv,
+            device=device,
+            config_override=config_override,
+            activation="silu",
+            use_1d_systolic_array=use_1d_systolic_array,
+            shard_layout=shard_layout,
+        )
+        self.cv2 = Yolov12x_Conv2D(
+            conv=parameter.cv2.conv,
+            conv_pth=conv_pt.cv2.conv,
+            device=device,
+            activation="silu",
+            use_1d_systolic_array=use_1d_systolic_array,
+            shard_layout=shard_layout,
+            config_override=config_override,
+        )
+
+        if is_bk_enabled:
+            self.m = Bottleneck(device, parameter[0], conv_pt.m[0])
+        else:
+            self.m_0 = C3k(device, parameter[0], conv_pt.m[0])
+            self.m_1 = C3k(device, parameter[1], conv_pt.m[1])
+
+    def __call__(self, x):
+        x = self.cv1(x)
+        if x.is_sharded():
+            x = ttnn.sharded_to_interleaved(x, ttnn.L1_MEMORY_CONFIG)
+
+        y1 = x[:, :, :, : x.shape[-1] // 2]
+        y2 = x[:, :, :, x.shape[-1] // 2 : x.shape[-1]]
+
+        if self.is_bk_enabled:
+            y3 = self.m(y2)
+            x = concat(-1, False, y1, y2, y3)
+        else:
+            y3 = self.m_0(y2)
+            y4 = self.m_1(y2)
+            x = concat(-1, False, y1, y2, y3, y4)
+
+        ttnn.deallocate(y1)
+        ttnn.deallocate(y2)
+        ttnn.deallocate(y3)
+
+        x = self.cv2(x)
+        return x